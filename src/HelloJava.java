4957830a2d4a5a049ac90a3cc5bbf2cc88376ecb
suha <gusuhawoo@gmail.com>
2024-02-12 19:41:04 +0900

    Add : System.out.println();

---
+++ src/HelloJava.java
@@ -0,0 +1,7 @@
+public class HelloJava {
+//  keyboard shortcut : psvm => public static void main
+  public static void main(String[] args) {
+//    keyboard shortcut : sout => System.out.println();
+    System.out.println("hello java");
+  }
+}
