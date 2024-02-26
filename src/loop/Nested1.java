076b529c9fcf1c7d2420d3a7da75f514ba689018
suha <gusuhawoo@gmail.com>
2024-02-26 09:11:15 +0900

    Add : NestedLoop1

---
+++ src/loop/Nested1.java
@@ -0,0 +1,28 @@
+package loop;
+
+public class Nested1 {
+  public static void main(String[] args) {
+    for (int i = 0; i < 2; i++){
+      System.out.println("외부 for 시작 i: " + i);
+      for (int j = 0; j < 3; j++){
+        System.out.println("→ 내부 for i: " + i + ", j: " + j);
+      }
+      System.out.println("외부 for 종료 i: " + i);
+      System.out.println();
+    }
+    /*
+    외부 for 시작 i: 0
+    → 내부 for i: 0, j: 0
+    → 내부 for i: 0, j: 1
+    → 내부 for i: 0, j: 2
+    외부 for 종료 i: 0
+
+    외부 for 시작 i: 1
+    → 내부 for i: 1, j: 0
+    → 내부 for i: 1, j: 1
+    → 내부 for i: 1, j: 2
+    외부 for 종료 i: 1
+
+    */
+  }
+}
