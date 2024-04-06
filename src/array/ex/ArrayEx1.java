b993898c67e35a1e51d4230745b0f0fc20fcb663
suha <gusuhawoo@gmail.com>
2024-04-06 15:54:32 +0900

    Add : ArrayEx1

---
+++ src/array/ex/ArrayEx1.java
@@ -0,0 +1,19 @@
+package array.ex;
+
+public class ArrayEx1 {
+  public static void main(String[] args) {
+    int[] students = {90,80,70,60,50};
+
+    int total = 0;
+//    for (int i = 0; i < students.length; i++) {
+//      total += students[i];
+//    }
+    for (int student : students){
+      total += student;
+    }
+    double average = (double) total / 5;
+
+    System.out.println("점수 총합: "+ total); // 점수 총합: 350
+    System.out.println("점수 평균: " + average); // 점수 평균: 70.0
+  }
+}
