e97747865f7506dc8e710efdfcf788d29bab5480
suha <gusuhawoo@gmail.com>
2024-08-07 09:11:28 +0900

    Add : Overloading3

---
+++ src/method/Overloading3.java
@@ -0,0 +1,22 @@
+package method;
+
+public class Overloading3 {
+  public static void main(String[] args) {
+    System.out.println("1: " + add(1,2));
+    System.out.println("2: " + add(1.2,1.5));
+  }
+
+  public static int add(int a, int b) {
+    System.out.println("1번 호출");
+    return a + b;
+  }
+
+  public static double add(double a, double b) {
+    System.out.println("2번 호출");
+    return a + b;
+  }
+}
+// 1번 호출
+// 1: 3
+// 2번 호출
+// 2: 2.7
